47cca21fc957ebcffd7e004ee3ef113a01f018b8
Agustinaschvartzberg <142595355+Agustinaschvartzberg@users.noreply.github.com>
2024-11-20 15:13:45 -0300

    cambios en la base de datos

--- .sql
+++ .sql
@@ -1,63 +1,56 @@
-CREATE SCHEMA proyectoIntegrador;
-USE proyectoIntegrador;
+CREATE SCHEMA baseDeDatos;
+USE baseDeDatos;
 
-/*TABLA USUARIOS*/
-CREATE TABLE usuarios (
-    id_usuario INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(100),
-    email VARCHAR(100),
-    contra VARCHAR(100) NOT NULL,
+CREATE TABLE users (
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    usuario VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    contraseña VARCHAR(100) NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deletedAt TIMESTAMP NULL
+    deletedAt TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP
 );
 
-/*TABLA PRODUCTOS*/
-CREATE TABLE productos (
-    id_producto INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    id_delUsuario INT UNSIGNED NOT NULL,
-    FOREIGN KEY (id_delUsuario) REFERENCES usuarios(id_usuario),
-    imagen_producto TEXT,
-    nombre VARCHAR(70),
-    descripcion VARCHAR(200),
+CREATE TABLE products (
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    imagen TEXT NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(300) NOT NULL,
+    id_user INT UNSIGNED, FOREIGN KEY (id_user) REFERENCES users(id),
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deletedAt TIMESTAMP NULL
+    deletedAt TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP 
 );
-
 
 
 /*USUARIOS*/
-INSERT INTO usuarios
-VALUES (DEFAULT, "agustina", "agustinaschvartzberg@gmail.com", "Hola12", NULL, NULL, NULL);
+INSERT INTO users
+VALUES (DEFAULT, "agustina", "agustinaschvartzberg@gmail.com", "Hola12", DEFAULT, DEFAULT, DEFAULT);
 
-INSERT INTO usuarios
-VALUES (DEFAULT, "delfina", "delfinamasri@gmail.com", "Chau12", NULL, NULL, NULL);
+INSERT INTO users
+VALUES (DEFAULT,  "delfina", "delfinamasri@gmail.com", "Chau12", DEFAULT, DEFAULT, DEFAULT);
 
-INSERT INTO usuarios
-VALUES (DEFAULT, "violeta", "violecheb@gmail.com", "Adios12", NULL, NULL, NULL);
+INSERT INTO users
+VALUES (DEFAULT,  "violeta","violecheb@gmail.com", "Adios12", DEFAULT, DEFAULT, DEFAULT);
 
-INSERT INTO usuarios
-VALUES (DEFAULT, "agus", "agusschv@gmail.com", "Bonjour12", NULL, NULL, NULL);
+INSERT INTO users
+VALUES (DEFAULT, "juan",  "juan@gmail.com", "Bonjour12", DEFAULT, DEFAULT, DEFAULT);
 
-INSERT INTO usuarios
-VALUES (DEFAULT, "delfina", "delfinamasri@gmail.com", "Hello12", NULL, NULL, NULL);
-
-
+INSERT INTO users
+VALUES (DEFAULT,  "federico", "fede@gmail.com", "Hello12", DEFAULT, DEFAULT, DEFAULT);
 
 /*PRODUCTOS*/
-INSERT INTO productos
-VALUES (DEFAULT, 1, "https://http2.mlstatic.com/D_NQ_NP_643641-MLU74717350223_022024-O.webp", "Paleta Dior", "producto multiusos que combina iluminador y colorete", NULL, NULL, NULL);
+INSERT INTO products
+VALUES (DEFAULT, "img-dior.jpg", "Paleta Dior", "Producto multiusos que combina iluminador y colorete", 1, DEFAULT, DEFAULT, DEFAULT);
 
-INSERT INTO productos
-VALUES (DEFAULT, 3, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoBTYNQFPK4Z0XPlyU8hhZv6AVc521JK1wLw&s", "Polvo compacto Chanel", "diseñado para suavizar, matificar y definir la piel con una textura ligera y de larga duración. ", NULL, NULL, NULL);
+INSERT INTO products
+VALUES (DEFAULT, "img-chanel.jpg", "Polvo compacto Chanel", "Diseñado para suavizar, matificar y definir la piel con una textura ligera y de larga duración. ", 2, DEFAULT, DEFAULT, DEFAULT);
 
-INSERT INTO productos
-VALUES (DEFAULT, 1, "https://http2.mlstatic.com/D_NQ_NP_643641-MLU74717350223_022024-O.webp", "Dyson Airwrap", "El único moldeador multifunción que seca, riza, da forma y elimina el encrespamiento con el efecto Coanda.", NULL, NULL, NULL);
+INSERT INTO products
+VALUES (DEFAULT, "img-dyson.jpg", "Dyson Airwrap", "El único moldeador multifunción que seca, riza, da forma y elimina el encrespamiento con el efecto Coanda.", 3, DEFAULT, DEFAULT, DEFAULT);
 
-INSERT INTO productos
-VALUES (DEFAULT, 4, "https://www.valentino-beauty.es/dw/image/v2/AAQP_PRD/on/demandware.static/-/Sites-val-master-catalog/default/dwc5592c66/images/pdp/MPL01076/3614273218047/Valentino-Very-Valentino-Foundation-LIGR1-3614273218047-Front.jpg?sw=270&sh=270&sm=cut&sfrm=jpg&q=70", "Base Valentino", "es un producto que se aplica en el rostro para crear una base uniforme y ayudar a que la piel luzca mejor", NULL, NULL, NULL);
+INSERT INTO products
+VALUES (DEFAULT, "img-valentino.jpg", "Base Valentino", "Es un producto que se aplica en el rostro para crear una base uniforme y ayudar a que la piel luzca mejor", 4, DEFAULT, DEFAULT, DEFAULT);
 
-INSERT INTO productos
-VALUES (DEFAULT, 5, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaCbu5pkmqx5QfAFOB3-WDwH-kBLxmqjMMDw&s", "Mascara Lancome", "es un cosmético usado para oscurecer, espesar, curvar y definir las pestañas.", NULL, NULL, NULL);
-
+INSERT INTO products
+VALUES (DEFAULT, "img-lancome.jpg", "Mascara Lancome", "Es un cosmético usado para oscurecer, espesar, curvar y definir las pestañas.", 5, DEFAULT, DEFAULT, DEFAULT);
